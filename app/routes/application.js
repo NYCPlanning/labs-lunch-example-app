f7aede215fac9518a77f14a989dd5c160c49f8c4
allthesignals <wilburnforce@gmail.com>
2018-03-07 12:03:16 -0500

    Added ember-cli-github-pages addon

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-const API = 'https://planninglabs.carto.com/api/v2/sql?skipfields=the_geom&q=SELECT%20*%20FROM%20cbbr_submissions_pts_fy18v5%20LIMIT%2010';
+const API = 'https://planninglabs.carto.com/api/v2/sql?skipfields=the_geom&q=SELECT%20*%20FROM%20cbbr_fy18_pts_v5%20LIMIT%2010';
 
 export default Route.extend({
   model() {
---
+++ app/utils/build-mapbox-paint-object.js
@@ -0,0 +1,3 @@
+export default function buildMapboxPaintObject() {
+  return true;
+}
