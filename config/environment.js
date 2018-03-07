c56527a65fdf5da6df70f1aa6745b2d6dd80dfeb
allthesignals <wilburnforce@gmail.com>
2018-03-07 12:10:32 -0500

    change rootURL

--- config/environment.js
+++ config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/budget-request-app/';
+    ENV.rootURL = '/labs-lunch-example-app/';
     // here you can enable a production-specific feature
   }
 
