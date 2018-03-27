451278a74cc43711fa24fa6682b42ac44e8dca3c
allthesignals <wilburnforce@gmail.com>
2018-03-27 10:38:10 -0400

    Get X and Y data

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -5,6 +5,6 @@ export default DS.JSONAPIAdapter.extend({
     return key;
   },
   urlForFindAll() {
-    return 'https://planninglabs.carto.com/api/v2/sql?skipfields=the_geom&q=SELECT%20 cartodb_id as id, *%20FROM%20cbbr_fy18_pts_v5%20LIMIT%2010';
+    return 'https://planninglabs.carto.com/api/v2/sql?skipfields=the_geom&q=SELECT%20 cartodb_id as id, st_x(the_geom) as x, st_y(the_geom) as y, *%20FROM%20cbbr_fy18_pts_v5%20LIMIT%2010';
   },
 });
